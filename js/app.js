5dcafc53646d80200643cb1db06ca1c6a58baae6
EllieKaun <ilyafazylbaev@mail.ru>
2024-04-01 19:16:50 +0600

    lesson 10

--- js/app.js
+++ js/app.js
@@ -1,22 +1,10 @@
-// // console.log("hello world");
+var mobileMenu = document.querySelector(".mobileMenu");
+var mobileMenuOpened = document.querySelector(".mobileMenuOpened");
 
-// "hello world";
-// "1";
-// 1;
-
-// console.log(1 + 2 - 10); //number
-// console.log("1" + "2"); //string
-// console.log("hello" + " " + "world"); //string
-
-// console.log(4 + "яблока");
-// console.log(10 - "2" + "яблока");
-// console.log("яблоко" + "яблоко");
-
-// var myVar = 10;
-// var b = myVar * 9;
-
-// console.log(b);
-
-var myName = "Ilya";
-
-console.log("Hello world my name is " + myName);
+console.log(mobileMenuOpened);
+mobileMenu.addEventListener("click", function () {
+  // var menu = document.createElement("div");
+  // menu.innerText = "text";
+  // mobileMenuOpened.append(menu);
+  mobileMenuOpened.classList.add("visible");
+});
